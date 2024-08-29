c11d12a3f172414f74e72f1282ecda1172a85e28
Ikenna John Agu <ike.john.agu@gmail.com>
2024-08-29 04:29:57 +0100

    Edited shell.h

--- shell.h
+++ shell.h
@@ -12,14 +12,77 @@
 #include <limits.h>
 #include <fcntl.h>
 
-#define MAX_C 10
-#define BUFSIZE 1024
-#define BUF_FLUSH -1
 
 #define READ_BUF_SIZE 1024
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
 
+#define CMD_NORM	0
+#define CMD_OR		1
+#define CMD_AND		2
+#define CMD_CHAIN	3
+
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+
+#define HIST_FILE	".simple_shell_history"
+#define HIST_MAX	4096
+
+extern char **environ;
+
+
+
+/**
+ *struct passinfo - This contains pseudo-arguements to pass into a function,
+ *allowing uniform prototype for function pointer struct
+ *@arg: This is the string generated from getline containing arguements
+ *@argv: This is an array of strings generated from arg
+ *@path: This is the string path for the current command
+ *@argc: This is the argument count
+ *@line_count: This is the error count
+ *@err_num: This is the error code for exit()s
+ *@linecount_flag: Only if on count this line of input
+ *@fname: This is the program filename
+ *@env: This is the linked list local copy of environ
+ *@environ: This is the custom modified copy of environ from LL env
+ *@history: This is the history node
+ *@alias: This is the alias node
+ *@env_changed: This is on if environ was changed
+ *@status: This is the return status of the last exec'd command
+ *@cmd_buf: This is the address of pointer to cmd_buf, on if chaining
+ *@cmd_buf_type: This is the CMD_type ||, &&, ;
+ *@readfd: This is the fd to read line input from
+ *@histcount: This is the history line number count
+ */
+typedef struct passinfo
+{
+	char *arg;
+	char **argv;
+	char *path;
+	int argc;
+	unsigned int line_count;
+	int err_num;
+	int linecount_flag;
+	char *fname;
+	list_t *env;
+	list_t *history;
+	list_t *alias;
+	char **environ;
+	int env_changed;
+	int status;
+
+	char **cmd_buf;
+	int cmd_buf_type;
+	int readfd;
+	int histcount;
+} info_t;
+
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+	0, 0, 0}
 
 int readmyCommand(void);
 
